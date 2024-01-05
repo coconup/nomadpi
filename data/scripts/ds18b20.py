8220bfe1fcbb246bc839356fe15aa5dc0d7dec1b
coconup <a.canedoli@gmail.com>
2024-01-05 11:38:13 +0100

    rewriting ds18b20 script

--- data/scripts/ds18b20.py
+++ data/scripts/ds18b20.py
@@ -8,38 +8,29 @@ status = 1
 def init():
     global tempSensors, status
     try:
-        for x in os.listdir("/sys/bus/w1/devices"):
-            if (x.split("-")[0] == "28") or (x.split("-")[0] == "10"):
-                tempSensors.append(x)
+        tempSensors = [x for x in os.listdir("/sys/bus/w1/devices") if x.split("-")[0] in {"28", "10"}]
     except:
-        # Auslesefehler
-        print ("Der Verzeichnisinhalt konnte nicht ausgelesen werden.")
+        print("Error accessing directory")
         status = 0
 
 def readValues():
     global tempSensors, tempReadings, status
-    x = 0
-    while x < len(tempSensors):
+    for sensor in tempSensors:
         try:
-            filename = "/sys/bus/w1/devices/" + tempSensors[x] + "/w1_slave"
-            file = open(filename)
-            filecontent = file.read()
-            file.close()
+            filename = f"/sys/bus/w1/devices/{sensor}/w1_slave"
+            with open(filename) as file:
+                filecontent = file.read()
             stringValue = filecontent.split("\n")[1].split(" ")[9]
             floatValue = float(stringValue[2:]) / 1000
             temperature = '%6.2f' % floatValue
-            tempReadings.insert(x, temperature)
-            x = x + 1
+            tempReadings.append(temperature)
         except:
-            tempReadings.insert(x, 'undefined')
-            x = x + 1
+            tempReadings.append('undefined')
 
 init()
 
 while status == 1:
-    x = 0
     readValues()
-    while x < len(tempSensors):
-        print (x, " ", tempReadings[x], " °C")
-        x = x + 1
-        status = 0
+    for x, reading in enumerate(tempReadings):
+        print(x, reading, "°C")
+    status = 0
