e7d58e2a36464e073002b2a480e685b952ea748f
Andrea Canedoli <andrea@hibase.co>
2024-03-04 16:41:40 +0100

    updating start script

--- start.sh
+++ start.sh
@@ -6,9 +6,9 @@ NC='\033[0m' # No Color
 docker-compose up -d
 
 echo -e "${BOLD}Services successfully started${NC}"
-echo -e "${GREEN}Frontend:           http://$(host).local:3000${NC}"
+echo -e "${GREEN}Frontend:           http://$(hostname).local:3000${NC}"
 echo -e "${GREY}---------------------------------------------${NC}"
-echo -e "Portainer:          http://$(host).local:9000"
-echo -e "Core API:           http://$(host).local:1880"
-echo -e "Automation API:     http://$(host).local:1881"
-echo -e "Frigate:            http://$(host).local:5000"
+echo -e "Portainer:          http://$(hostname).local:9000"
+echo -e "Core API:           http://$(hostname).local:1880"
+echo -e "Automation API:     http://$(hostname).local:1881"
+echo -e "Frigate:            http://$(hostname).local:5000"
